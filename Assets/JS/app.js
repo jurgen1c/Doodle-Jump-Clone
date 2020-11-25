7d18d099a54764e139925ad366cc62dcc512c5b9
jurgen1c <jurgen1c@gmail.com>
2020-11-24 22:48:21 -0600

    Add platform & doodle movement

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -2,9 +2,12 @@ document.addEventListener('DOMContentLoaded', () =>{
   const grid = document.querySelector('.grid');
   const doodler = document.createElement('div');
   let doodlerLeftSpace = 50;
-  let doodlerBottomSpace = 150;
+  let doodlerBottomSpace = 250;
   let isGameOver = false;
   let platformCount = 5;
+  let platforms = [];
+  let upTimerId;
+  let downTimerId;
 
   function createDoodler(){
     grid.appendChild(doodler);
@@ -31,14 +34,54 @@ document.addEventListener('DOMContentLoaded', () =>{
     for (let i = 0; i < platformCount; i++){
       let platformGap = 600 / platformCount;
       let newPlatBottom = 100 + i * platformGap;
-      let newPlatform = new Platform;
+      let newPlatform = new Platform(newPlatBottom);
+      platforms.push(newPlatform);
     }
   }
-  
+
+  function movePlatforms(){
+    if (doodlerBottomSpace > 200){
+      platforms.forEach(platform => {
+        platform.bottom -= 4;
+        let visual = platform.visual;
+        visual.style.bottom = platform.bottom +'px'
+      })
+    }
+  }
+  function gameOver(){
+    console.log('game over');
+    isGameOver = true;
+    clearInterval(upTimerId);
+    clearInterval(downTimerId);
+  }
+
+  function fall(){
+    clearInterval(upTimerId);
+    downTimerId = setInterval(function () {
+      doodlerBottomSpace -= 10;
+      doodler.style.bottom = doodlerBottomSpace + 'px';
+      if (doodlerBottomSpace <= 0){
+        gameOver();
+      }
+    })
+  }
+
+  function jump(){
+    clearInterval(downTimerId);
+    upTimerId = setInterval(function (){
+      doodlerBottomSpace += 20;
+      doodler.style.bottom = doodlerBottomSpace + 'px';
+      if ( doodlerBottomSpace > 350){
+        fall();
+      }
+    }, 30)
+  }
+
   function start(){
     if (!isGameOver){
       createDoodler();
-      createPlataforms();
+      createPlatforms();
+      setInterval(movePlatforms, 30);
     }
   }
   start();
